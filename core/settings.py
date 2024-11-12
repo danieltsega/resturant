4a8bb0c361bcde0bd68dd4a0034dce0f1e71d87c
danieltsega <danieltsega6658@gmail.com>
2024-11-12 18:00:42 +0300

    feat: add templates dir to the settings

--- core/settings.py
+++ core/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,3 +130,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# For development, use the console email backend
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
